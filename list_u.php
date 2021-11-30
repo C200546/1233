2cba2b2fba735f574ddc7c0b743edbc011cabcab
C200546 <67032922+C200546@users.noreply.github.com>
2021-11-30 16:23:41 +0900

    Update list_u.php

--- list_u.php
+++ list_u.php
@@ -55,8 +55,7 @@ try{
 <p>データ件数：<?php echo $count;?>件</p>
 
 <div class="container">
-/*  (1)ここに適切な formタグを追加してください  */
-
+<form action="" method="POST">
 <table border=1>
     <tr><th>id</th><th>名前</th><th>メールアドレス</th><th>性別</th><th>選択対象</th></tr>
     <?php foreach($data as $row): ?>
@@ -76,7 +75,7 @@ try{
         ?>
     </td>
     <td>
-        /*  (2)ここにラジオボタン表示処理を追加して下さい  */
+        <input type = "radio" value = "編集" >編集
 
     </td>
     </tr>
